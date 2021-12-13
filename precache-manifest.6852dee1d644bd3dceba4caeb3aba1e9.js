af39b31675ddb293b3e772724add02781c7ec8ab
alex <1220525493@qq.com>
2021-12-13 15:58:24 +0800

    deploy

---
+++ precache-manifest.6852dee1d644bd3dceba4caeb3aba1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b426d98508b3466d91",
+    "url": "/money-1-website/css/app.f8e531d1.css"
+  },
+  {
+    "revision": "c1d3f1512f6c73deed2578a2b607260a",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "e7b426d98508b3466d91",
+    "url": "/money-1-website/js/app.b612c34c.js"
+  },
+  {
+    "revision": "34f08e3be3746a689233",
+    "url": "/money-1-website/js/chunk-vendors.ecd0f110.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/money-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-website/robots.txt"
+  }
+]);
